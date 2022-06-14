a9e84332a8c6c2eddb8ff79377ee4428a58898f8
skhassandaily <sk.hassan@dailymotion.com>
2022-06-14 16:45:33 +0530

    update docs

---
+++ examples/embed_gam/main.js
@@ -0,0 +1,56 @@
+
+
+const ceCode = document.getElementById("ceCode");
+const gamCode = document.getElementById("gamCode");
+const pageCode = document.getElementById("pageCode");
+const convertSimple = document.getElementById("convertSimple");
+const convertSize = document.getElementById("convertSize");
+
+loadTheCode(`<script>
+        var tw = window.top, sfe = window.frameElement,dmScript;
+        sfe.parentNode.insertAdjacentHTML("beforeBegin", "<div class='dm-player' owners='suaradotcom' sort='recent' playerId='x4aj'></div>");
+        dmScript= document.createElement('script');
+        dmScript.type = 'text/javascript';
+        dmScript.src = 'https://srvr.dmvs-apac.com/v2/dm-ce.min.js';
+        tw.document.body.appendChild(dmScript);
+    </script>`)
+
+
+convertSimple.addEventListener("click",()=>{
+    loadTheCode(`<script>
+    var tw = window.top, sfe = window.frameElement,dmScript;
+    sfe.parentNode.insertAdjacentHTML("beforeBegin", "${ceCode.innerText}");
+    dmScript= document.createElement('script');
+    dmScript.type = 'text/javascript';
+    dmScript.src = 'https://srvr.dmvs-apac.com/v2/dm-ce.min.js';
+    tw.document.body.appendChild(dmScript);
+    </script>`);
+    loadThePageCode(false);
+});
+
+convertSize.addEventListener("click",()=>{
+    loadTheCode(`<script>
+    var tw = window.top, sfe = window.frameElement,dmScript;
+    tw.document.querySelector(".playerContainer").innerHTML = ("${ceCode.innerText}");
+    dmScript= document.createElement('script');
+    dmScript.type = 'text/javascript';
+    dmScript.src = 'https://srvr.dmvs-apac.com/v2/dm-ce.min.js';
+    tw.document.body.appendChild(dmScript);
+    </script>`);
+
+    loadThePageCode(true);
+});
+
+
+function loadTheCode(code){
+    gamCode.innerText = code;
+}
+
+function loadThePageCode(flag){
+    if(flag){
+        pageCode.style.display = "block";
+    }else{
+        pageCode.style.display = "none";
+    }
+}
+
--- hidden_features.md
+++ hidden_features.md
@@ -11,6 +11,7 @@ We developed a few extra parameters for video embed contexts requiring bespoke f
 | hideButton | boolean | This is a custom button for hiding the PiP Player instead of closing it permanently. The visitor can unhide it when they want to watch it again. This will override the original controls. |
 | showPipButtonAfter | number | This is a custom param that controls when the hide button will show once an ad creative is displayed. You can define the number in seconds. By default, if you don't put any number it will use default 5 seconds. |
 | smallPip | boolean | This is a custom param that change PIP size for mobile view with custom controls over PIP. [Sample page](https://dmvs-apac.github.io/custom-embed-v2/examples/small_pip/)|
+| omidSDK | boolean |  To avoid a policy violation with Google’s OMID verification script, set it `true`. [More on OMID here FYI](https://iabtechlab.com/standards/open-measurement-sdk/)|
 
 If you don't want to use this, you just don't need to use the parameters inside the container.
 
