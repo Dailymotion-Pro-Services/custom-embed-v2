72a03e65cb0cb67c1c29e860797ef86548362622
skhassandaily <75718940+skhassandaily@users.noreply.github.com>
2024-03-27 11:58:42 +0530

    Update script url

--- examples/embed_gam/main.js
+++ examples/embed_gam/main.js
@@ -11,7 +11,7 @@ loadTheCode(`<script>
         sfe.parentNode.insertAdjacentHTML("beforeBegin", "<div class='dm-player' owners='suaradotcom' sort='recent' playerId='x4aj'></div>");
         dmScript= document.createElement('script');
         dmScript.type = 'text/javascript';
-        dmScript.src = 'https://srvr.dmvs-apac.com/v2/dm-ce.min.js';
+        dmScript.src = 'https://statics.dmcdn.net/c/dm-ce.min.js';
         tw.document.body.appendChild(dmScript);
     </script>`)
 
@@ -34,7 +34,7 @@ convertSize.addEventListener("click",()=>{
     tw.document.querySelector(".playerContainer").innerHTML = ("${ceCode.innerText}");
     dmScript= document.createElement('script');
     dmScript.type = 'text/javascript';
-    dmScript.src = 'https://srvr.dmvs-apac.com/v2/dm-ce.min.js';
+    dmScript.src = 'https://statics.dmcdn.net/c/dm-ce.min.js';
     tw.document.body.appendChild(dmScript);
     </script>`);
 
